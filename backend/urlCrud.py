6c8290e9be253fcfab658a2398230a8dd20c0fe7
Julian Arce <juarce@itba.edu.ar>
2022-07-10 20:04:44 -0300

    feat: get url improvements

--- backend/urlCrud.py
+++ backend/urlCrud.py
@@ -3,11 +3,23 @@ import redis
 r = redis.Redis(host='localhost', port=6379, db=0)
 
 
+class Url:
+    def __init__(self, d=None):
+        if d is not None:
+            self.url = d['url']
+            self.owner = int(d['owner'])
+            self.clicks = int(d['clicks'])
+
+
 def get_url(key: str):
-    url_object = r.hgetall(key)
-    if url_object:
-        r.hincrby(key, 'clicks', 1)
-    return url_object
+    url = r.hget(key, 'url')
+    if url is None:
+        return
+    r.hincrby(key, 'clicks', 1)
+    h_url = r.hgetall(key)
+    h_url = {y.decode('ascii'): h_url.get(y).decode('ascii') for y in h_url.keys()}
+    return Url(h_url)
+
 
 # TODO
 def get_urls_by_username():
@@ -28,3 +40,4 @@ def create_url(key: str, url: str, owner: int):
 
 def delete_url(key: str):
     r.hdel(key, 'url', 'owner', 'clicks')
+
