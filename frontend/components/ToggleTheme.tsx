0f6fe9a95d9d53e801017037bf3d33c7e3d99404
Gian <gpecile@itba.edu.ar>
2022-07-14 15:12:17 -0300

    fix: hydration error

--- frontend/components/ToggleTheme.tsx
+++ frontend/components/ToggleTheme.tsx
@@ -1,10 +1,22 @@
 import {useTheme} from "next-themes";
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
+import {useState, useEffect} from 'react'
 
 export const ToggleTheme = () => {
     const {theme, setTheme} = useTheme();
-    return (<button className="transition-all ease-linear" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
-        {theme === 'dark' ? < DarkModeIcon/> : <LightModeIcon/>}
+    const [mounted, setMounted] = useState(false)
+
+    // useEffect only runs on the client, so now we can safely show the UI
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return null
+    }
+
+    return (<button className="transition-all duration-300 ease-linear" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+        {theme === 'dark' ? <DarkModeIcon/> : <LightModeIcon/>}
     </button>);
 }
