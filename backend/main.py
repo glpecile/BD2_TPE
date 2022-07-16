b0f58fb3cd29273ced9ee44e562e49a29e702e1b
Julian Arce <juarce@itba.edu.ar>
2022-07-16 17:49:15 -0300

    chore: allow cors

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes.urls import routes_url
 from routes.users import routes_user
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8080"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"]
+)
 
 app.include_router(routes_user, prefix="/users")
 app.include_router(routes_url, prefix="/url")
