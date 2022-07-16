88b6c64c66f7f06fbcaed910ffb7815db597ec4f
Julian Arce <juarce@itba.edu.ar>
2022-07-16 15:23:52 -0300

    fix: url endpoints

--- backend/routes/urls.py
+++ backend/routes/urls.py
@@ -3,24 +3,22 @@ from schemas.url import Url
 
 import urlCrud
 
-
 routes_url = APIRouter()
 
 
-@routes_url.get("/{key}")
-def get_url(key: str):
-    return urlCrud.get_url(key)
-
-
-# TODO change to query param
-@routes_url.get("/user/{username}")
-def get_urls_by_user(user: int):
-    return urlCrud.get_urls_by_username(user)
+@routes_url.get("")
+def get_urls_by_user(owner: int):
+    return urlCrud.get_urls_by_user(owner)
 
 
 @routes_url.post("", response_model=Url)
 def create_url(key: str, url: str, owner: int):
     return urlCrud.create_url(key, url, owner)
+
+
+@routes_url.get("/{key}")
+def get_url(key: str):
+    return urlCrud.get_url(key)
 
 
 @routes_url.delete("/{key}")
